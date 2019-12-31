f4d9ee7556102307dd6705c1f1d0f717bd1701a1
Jeff Jiang <jeffjiang8@gmail.com>
2019-12-31 13:45:10 -0500

    item crud backend

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,9 +10,26 @@ class Api::V1::ItemsController < ApplicationController
             category_id: params[:category_id],
             name: params[:name],
             packed: params[:packed],
-            user_id: params[:user_id],
+            user_id: params[:user_id]
         })
 
         render json: item
+    end
+
+    def update
+        item = Item.find(params[:id])
+        item.update( trip_id: params[:trip_id],
+                        category_id: params[:category_id],
+                        name: params[:name],
+                        packed: params[:packed],
+                        user_id: params[:user_id])
+        render json: item
+    end
+
+    def destroy
+        Item.find(params[:id]).destroy
+        
+
+        render json: Item.all
     end
 end
